43e0fc5fee1cb8c4c7c60960d4010a99a7e4e9db
freestyledork <freestyledork@gmail.com>
2018-03-11 23:42:31 -0400

    start collection model queries

--- vendor/freestyledork/phergie-irc-plugin-react-coins/src/Model/CollectionModel.php
+++ vendor/freestyledork/phergie-irc-plugin-react-coins/src/Model/CollectionModel.php
@@ -13,4 +13,36 @@ class CollectionModel extends UserModel
     protected $minCollectInterval = 120;
     protected $baseCollectAmount = 200;
 
+    public function addNewCollection($user_id, $amount){
+        $statement = $this->connection->prepare(
+            'INSERT INTO collections (user_id, amount) VALUES (?,?)'
+        );
+        $statement->execute([ $user_id,$amount ]);
+    }
+
+    public function getUserTotalCollections($user_id){
+        $statement = $this->connection->prepare(
+            'SELECT COUNT(*)
+                        FROM collections
+                       WHERE user_id = ?'
+        );
+        if ($statement->execute([ $user_id ])) {
+            $result = $statement->fetch();
+        }
+        return $result;
+    }
+
+    public function getUserLastCollectionInfo($user_id){
+        $statement = $this->connection->prepare(
+            'SELECT *
+                        FROM collections
+                       WHERE user_id = ?
+                   ORDER  BY time DESC
+                       LIMIT 1'
+        );
+        if ($statement->execute([ $user_id ])) {
+            $result = $statement->fetch();
+        }
+        return $result;
+    }
 }
